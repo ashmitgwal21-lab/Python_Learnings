8a095a7077a57bab5268d9ff6b05fc1dfb7c88fd
ashmitgwal21-lab <ashmitgwal21@gmail.com>
2026-04-22 02:18:49 +0530

    Add files via upload

---
+++ String_Methods.py
@@ -0,0 +1,9 @@
+text = " Live the moment "
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+print(text.strip())
+print("Hello , World".replace("Hello","bye bye"))
+print("apple".find("p"))
+print("Ashmitt".count("t"))
