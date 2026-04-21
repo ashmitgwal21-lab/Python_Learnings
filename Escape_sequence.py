f3f8ac8154d75ba41c9bd0e69452372a06e9adef
ashmitgwal21-lab <ashmitgwal21@gmail.com>
2026-04-22 01:42:43 +0530

    Add files via upload

---
+++ Escape_sequence.py
@@ -0,0 +1,13 @@
+"""
+\n- Newline
+\t - Tab
+\\ - Backslash
+\' - Single Quote
+\" - Double Quote
+"""
+
+print("Hello World.\nBye bye")
+print("Name:\tAlice")
+print("c:\\User\\work")
+print('Ashmit\'s Work')
+print("I am a \"Hero\".")
