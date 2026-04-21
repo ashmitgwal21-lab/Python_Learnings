1b51974d0b57effcfb6e69617e154b4ad30f8398
ashmitgwal21-lab <ashmitgwal21@gmail.com>
2026-04-22 02:03:49 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,11 @@
+p1 = "hello" + " " + "World"
+print(p1)
+
+fn1 = input("Please enter your first name: ")
+sn1 = input("Please enter your second name: ")
+print(f'Yous Name is {fn1} {sn1}')
+
+print("python"*3)
+
+fake_str = "Live the moment"
+print(len(fake_str))
